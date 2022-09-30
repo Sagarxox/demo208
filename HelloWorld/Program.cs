470b14ec85047d060c260b8fc124ecd071ff2c69
sagar <skatahre@gmail.com>
2022-09-30 08:33:43 +0530

    test

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿
+namespace HelloWorld
+{
+	class Program
+	{
+
+		public static void Main(string[] args)
+		{
+			int a = 15;
+			double b = 5.5;
+			bool c = false;
+			string s = "thjtuj";
+			Console.WriteLine("a + b" +a +b);
+			Console.WriteLine(" +s " +s +c);
+			
+	
+			Console.WriteLine("Hello, World!");
+			int userinput = Convert.ToInt32(  Console.ReadLine());
+			Console.WriteLine(userinput);
+
+        }
+	}
+}
